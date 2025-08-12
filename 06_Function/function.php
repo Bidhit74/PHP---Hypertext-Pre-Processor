92769c39d526a425c46beb25a5c64b1c5356b5d2
Bidhit <bidhitkumar61@gmail.com>
2025-08-12 22:50:53 +0530

    Add function examples and documentation in function.php and readme.md

---
+++ 06_Function/function.php
@@ -0,0 +1,13 @@
+<?php
+// 1. Built-in Functions – Predefined in PHP.
+    echo strlen("Hello"); // Output: 5
+    echo "<br>";
+    
+// 2. User-defined Functions – Created by the user.
+
+    function greet($name) {
+        echo "Hello, $name!";
+    }
+    greet("Bidhit"); // Output: Hello, Bidhit!
+
+?>
---
+++ 06_Function/readme.md
@@ -0,0 +1,11 @@
+# Function in PHP
+
+-   A function is a block of code that runs when called.
+
+-   Use: To reuse code, reduce repetition, and make programs organized.
+
+# Types of Functions
+
+1. Built-in Functions – Predefined in PHP.
+
+2. User-defined Functions – Created by the programmer.
