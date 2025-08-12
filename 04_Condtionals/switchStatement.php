bd9b00547404e8ce1d1e2b49452b75c930c6552e
Bidhit <bidhitkumar61@gmail.com>
2025-08-12 22:28:44 +0530

    Add switch statement example in switchStatement.php

---
+++ 04_Condtionals/switchStatement.php
@@ -0,0 +1,13 @@
+<?php
+$day = "Monday";
+    switch ($day) {
+        case "Monday":
+            echo "Start of the week.";
+            break;
+        case "Friday":
+            echo "Almost weekend.";
+            break;
+        default:
+            echo "Midweek day.";
+    }
+?>
