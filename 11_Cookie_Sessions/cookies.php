f939fae3e2931f4880c698e1b767e392e2fdf54a
Bidhit <bidhitkumar61@gmail.com>
2025-08-23 00:41:28 +0530

    Cookie delete

--- 11_Cookie_Sessions/cookies.php
+++ 11_Cookie_Sessions/cookies.php
@@ -2,11 +2,16 @@
     // Cookie set karna (expire time = 1 hour from now)
     // setcookie(name, value, expire, path, domain, secure, httponly);
     setcookie("username", "Bidhit", time() + 3600, "/");  
+    echo "Cookie set ho gayi!<br>";
+    
+    // Cookie delete karna (expire time ko past me set karte hain)
+    setcookie("username", "", time() - 3600, "/"); 
 
     // Cookie check aur print karna
     if(isset($_COOKIE["username"])) {
+
         echo "Welcome, " . $_COOKIE["username"];  // Agar cookie set hai to print karega
     } else {
-        echo "No cookie found!";  // Agar cookie abhi set nahi hai
+        echo "Cookie deleted successfully!";
     }
 ?>
