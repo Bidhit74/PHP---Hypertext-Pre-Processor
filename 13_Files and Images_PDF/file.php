a2b7ec44f2185fd5827c8bd17ef634fdd9fb839e
Bidhit <bidhitkumar61@gmail.com>
2025-08-23 09:55:10 +0530

    File Read and right

---
+++ 13_Files and Images_PDF/file.php
@@ -0,0 +1,25 @@
+<?php
+    // 1. File Open (write mode: 'w' => overwrite karega)
+    // $file = fopen("example.txt", "w"); 
+    // echo "File Created <hr>";
+
+    // 2. File Write
+    // fwrite($file, "Hello, this is first line.\n");
+    // fwrite($file, "PHP File Handling Example.\n");
+    // echo "File Write inside the file <hr>";
+
+    // 3. File Close
+    // fclose($file);
+
+    // 4. File Open (read mode: 'r')
+    $file = fopen("example.txt", "r") or die("unable to read file");
+
+    // 5. File Read (filesize() se pura content read hoga)
+    $content = fread($file, filesize("example.txt"));
+
+    // Output file content
+    echo nl2br($content);  // nl2br() new line ko <br> me convert karega
+
+    // 6. Close File
+    fclose($file);
+?>
---
+++ 13_Files and Images_PDF/readme.md
@@ -0,0 +1,27 @@
+# 📂 PHP File Handling
+
+-   PHP me file handling ka use data ko read, write, append, delete karne ke liye hota hai.
+
+# 🔑 Important Functions:
+
+-   fopen() – file open karne ke liye
+
+-   fread() – file read karne ke liye
+
+-   fwrite() – file me write karne ke liye
+
+-   fclose() – file close karne ke liye
+
+-   file_exists() – check karta hai file exist karti hai ya nahi
+
+-   File delete karne ke liye unlink() function use hota hai.
+
+# 📌 Modes in fopen()
+
+-   'r' → Read only
+
+-   'w' → Write (overwrite karega agar file pehle se hai)
+
+-   'a' → Append (purane data ke sath naya add karega)
+
+-   'x' → Create new file (agar pehle se hai to error)
