6683f4557dcf72e16095c1176669d2cf09cad677
Bidhit <bidhitkumar61@gmail.com>
2025-08-23 11:41:36 +0530

    Convert Image Type (JPG → PNG)

--- 13_Files and Images_PDF/image2.php
+++ 13_Files and Images_PDF/image2.php
@@ -1,15 +1,28 @@
 <!-- Load Existing Image -->
 <?php
-    $imagePath = __DIR__ . "/sample.jpg"; // safer path
+//     $imagePath = __DIR__ . "/sample.jpg"; // safer path
 
-    if (!file_exists($imagePath)) {
-        die("Image not found!");
-    }
+//     if (!file_exists($imagePath)) {
+//         die("Image not found!");
+//     }
 
-    $image = imagecreatefromjpeg($imagePath);
+//     $image = imagecreatefromjpeg($imagePath);
 
-    header("Content-Type: image/jpeg"); // no echo before this
-    imagejpeg($image);
+//     header("Content-Type: image/jpeg"); // no echo before this
+//     imagejpeg($image);
 
-    imagedestroy($image);
+//     imagedestroy($image);
+
+//Convert Image Type (JPG → PNG)
+
+// Load JPG
+$image = imagecreatefromjpeg(__DIR__ . "/sample.jpg");
+
+// Save as PNG
+imagepng($image, "converted.png");
+
+echo "Image converted successfully!";
+imagedestroy($image);
+
+
 ?>
