76ddf1fb246ec9a8a7cedd76252a0aa2c0251e26
Bidhit <bidhitkumar61@gmail.com>
2025-08-23 10:23:08 +0530

    Using opendir() and readdir()

--- 13_Files and Images_PDF/getFolder.php
+++ 13_Files and Images_PDF/getFolder.php
@@ -1,12 +1,14 @@
 <!-- PHP me ek folder (directory) ke andar ki files list karne ke liye scandir() ya opendir() functions ka use hota hai. -->
 
-<!-- 📂 Example 1: Using scandir() -->
+<!-- Dono tarike sahi hain, lekin scandir() simple aur fast hai. -->
+
+<!-- Example 1: Using scandir() -->
 
 <?php
     $folder = "uploads"; // folder ka naam
 
     // Get all files and folders inside "uploads"
-    $files = scandir($folder);
+    // $files = scandir($folder);
     // print_r($files)
 
     // Loop through files
@@ -19,12 +21,24 @@
 
     // Or
     // Files inside Remove (.) (..)
-    $files = array_diff($files, array(".",".."));
-    print_r($files);
-    foreach($files as $file) {
-        // echo  "<hr>" . $file;
-        // use <a> tag for file open
-        echo  "<hr>";
-        echo "<a href=./uploads/$file> $file </a>";
+    // $files = array_diff($files, array(".",".."));
+    // print_r($files);
+    // foreach($files as $file) {
+    //     // echo  "<hr>" . $file;
+    //     // use <a> tag for file open
+    //     echo  "<hr>";
+    //     echo "<a href=./uploads/$file> $file </a>";
+    // }
+
+    // Example 2: Using opendir() and readdir()
+    if (is_dir($folder)) {
+    if ($dh = opendir($folder)) {
+        while (($file = readdir($dh)) !== false) {
+            if ($file != "." && $file != "..") {
+                echo $file . "<br>";
+            }
+        }
+        closedir($dh);
     }
+}
 ?>
