f6a19bb7efef9ee2c5c1be9dc2308e3f4313c02a
Bidhit <bidhitkumar61@gmail.com>
2025-08-23 10:19:45 +0530

    Using scandir() - get folder

---
+++ 13_Files and Images_PDF/getFolder.php
@@ -0,0 +1,30 @@
+<!-- PHP me ek folder (directory) ke andar ki files list karne ke liye scandir() ya opendir() functions ka use hota hai. -->
+
+<!-- 📂 Example 1: Using scandir() -->
+
+<?php
+    $folder = "uploads"; // folder ka naam
+
+    // Get all files and folders inside "uploads"
+    $files = scandir($folder);
+    // print_r($files)
+
+    // Loop through files
+    // foreach ($files as $file) {
+        // Ignore current (.) and parent (..)
+    //     if ($file != "." && $file != "..") {
+    //         echo $file . "<br>";
+    //     }
+    // }
+
+    // Or
+    // Files inside Remove (.) (..)
+    $files = array_diff($files, array(".",".."));
+    print_r($files);
+    foreach($files as $file) {
+        // echo  "<hr>" . $file;
+        // use <a> tag for file open
+        echo  "<hr>";
+        echo "<a href=./uploads/$file> $file </a>";
+    }
+?>
